46456ac25c798a61a4d612b69d832397c73bc904
Pin Che <tan,pinche@kabam.ai>
2023-04-28 09:51:38 +0800

    removed the checking of range values to be within bounds of min and max

--- range_sensor_layer/src/range_sensor_layer.cpp
+++ range_sensor_layer/src/range_sensor_layer.cpp
@@ -236,12 +236,12 @@ void RangeSensorLayer::processFixedRangeMsg(sensor_msgs::Range& range_message)
 
 void RangeSensorLayer::processVariableRangeMsg(sensor_msgs::Range& range_message)
 {
-  if (range_message.range < range_message.min_range || range_message.range > range_message.max_range)
-    return;
+  // if (range_message.range < range_message.min_range || range_message.range > range_message.max_range)
+  //   return;
 
   bool clear_sensor_cone = false;
 
-  if (range_message.range == range_message.max_range){
+  if (range_message.range >= range_message.max_range){
     if(clear_on_max_reading_){
       clear_sensor_cone = true;
       }
